cb3dc68fb865e8bbaa7628c01d5e8fe959e4656e
Antonia Elek <antoniaelek@hotmail.com>
2019-03-15 23:01:31 +0100

    Callback for year selection

--- app.py
+++ app.py
@@ -3,11 +3,12 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 import dash_bootstrap_components as dbc
+from dash.dependencies import Input, Output
 
 from data.scrobbles import get_scrobbles
 from data.artists import get_artists
 
-from plots.plays_by_artist import get_artists_plays_at_work_plot
+from plots.plays_by_artist import get_artists_plays_at_work_plot, get_artists_plays_data
 from plots.plays_by_artist import get_artists_plays_at_work_data
 from plots.plays_by_artist import get_artists_plays_late_at_night
 from plots.plays_by_artist import get_artists_plays_on_weekends
@@ -22,13 +23,15 @@ def top_artist_div(title, data, id, align_left=True):
         img_class = 'col-lg-6 col-md-12 justify-content-lg-start justify-content-center d-flex'  # left-tab behind'
         div_text_large_class = 'd-none d-lg-block col-lg-6 justify-content-lg-start text-right'  # right-tab in-front'
 
-    div_img = html.Div(className=img_class)
+    div_img = html.Div(className=img_class, children=[
+            html.Img(className='gridbox', src='https://lastfm-img2.akamaized.net/i/u/174s/c6f59c1e5e7240a4c0d427abd71f3dbb')
+    ])
     div_text_large = html.Div(className=div_text_large_class, children=[
-        html.H4(title),
-        html.H2("no scrobbles")
+        html.H4(title, className='textbox'),
+        html.H2("No Scrobbles")
     ])
     div_text_small = html.Div(className='d-xl-none col-md-12', children=[
-        html.H3("no scrobbles")
+        html.H3("No Scrobbles")
     ])
     div_title_small = html.Div(className='d-lg-none col-md-12 pad', children=[
         html.H3(title, className="textbox")
@@ -76,39 +79,34 @@ scrobbles = get_scrobbles()
 print("Fetching artists...")
 artists = get_artists()
 
-print("Fetching top tags...")
-top_tags_trace, top_tags_layout = top_tags_plot(color=colors[0])
+# print("Fetching top tags...")
+# top_tags_trace, top_tags_layout = top_tags_plot(color=colors[0])
+#
+# print("Fetching top artists...")
+# top_artists_trace, top_artists_layout = top_artists_plot(color=colors[1])
 
-print("Fetching top artists...")
-top_artists_trace, top_artists_layout = top_artists_plot(color=colors[1])
-
-print("Fetching top artists at work...")
-top_at_work = get_artists_plays_at_work_data(scrobbles, artists)
-
-print("Fetching top late night artists...")
-top_late_night = get_artists_plays_late_at_night(scrobbles, artists)
-
-print("Fetching top weekend artists...")
-top_weekends = get_artists_plays_on_weekends(scrobbles, artists)
-
-work_div = top_artist_div("At Work", top_at_work, "top-work")
-weekends_div = top_artist_div("On Weekends", top_weekends, "top-weekends", align_left=False)
-late_night_div = top_artist_div("Late At Night", top_late_night, "top-late-night")
 
 navbar = dbc.NavbarSimple(
     children=[
-        dbc.NavItem(dbc.NavLink("Top Artists", href="#")),
-#        dbc.DropdownMenu(
-#            nav=True,
-#            in_navbar=True,
-#            label="Menu",
-#            children=[
-#                dbc.DropdownMenuItem("Entry 1"),
-#                dbc.DropdownMenuItem("Entry 2"),
-#                dbc.DropdownMenuItem(divider=True),
-#                dbc.DropdownMenuItem("Entry 3"),
-#            ],
-#        ),
+        # dbc.NavItem(dbc.NavLink("Top Artists", href="#")),
+        dcc.Dropdown(
+            id='year-input',
+            options=[{'label': str(i), 'value': str(i)} for i in scrobbles['Year'].unique()],
+            value='2019',
+            clearable=False,
+            style={'bgcolor':'#f8f9fa','bordercolor':'#f8f9fa'}
+        ),
+
+        # dbc.DropdownMenu(
+        #    nav=True,
+        #    in_navbar=True,
+        #    label="Year",
+        #    children=[
+        #        dbc.DropdownMenuItem("Entry 1"),
+        #        dbc.DropdownMenuItem("Entry 2"),
+        #        dbc.DropdownMenuItem("Entry 3"),
+        #    ],
+        #  ),
     ],
     brand="Last Dash",
     brand_href="#",
@@ -118,14 +116,7 @@ navbar = dbc.NavbarSimple(
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 app.layout = html.Div(className='text-center', children=[
-    # Title
     navbar,
-
-    # html.Div(className='row', children=[
-    #     html.Div(className='col-md-12', children=[
-    #         html.H1('Last Dash', className="")
-    #     ])
-    # ]),
 
     # Top artist at work, on weekends and late at night
     html.Div(className='row', children=[
@@ -133,10 +124,35 @@ app.layout = html.Div(className='text-center', children=[
             html.H1('Your top artists', className='')
         ])
     ]),
-    work_div,
-    weekends_div,
-    late_night_div
+
+    html.Div(id="content", children=[])
 ])
+
+@app.callback(
+    Output(component_id='content', component_property='children'),
+    [Input(component_id='year-input', component_property='value')]
+)
+def update_output_div(input_value):
+    scrobbles_selected = scrobbles[scrobbles['Year'] == int(input_value)]
+
+    print("Fetching top artists overall...")
+    overall = get_artists_plays_data(scrobbles_selected, artists)
+    overall_div = top_artist_div("Overall", overall, "total")
+
+    print("Fetching top artists at work...")
+    at_work = get_artists_plays_at_work_data(scrobbles_selected, artists)
+    at_work_div = top_artist_div("At Work", at_work, "top-work", align_left=False)
+
+    print("Fetching top late night artists...")
+    late_night = get_artists_plays_late_at_night(scrobbles_selected, artists)
+    late_night_div = top_artist_div("Late At Night", late_night, "top-late-night")
+
+    print("Fetching top weekend artists...")
+    weekends = get_artists_plays_on_weekends(scrobbles_selected, artists)
+    weekends_div = top_artist_div("On Weekends", weekends, "top-weekends", align_left=False)
+
+    return [overall_div, at_work_div, late_night_div, weekends_div]
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
--- plots/plays_by_artist.py
+++ plots/plays_by_artist.py
@@ -1,16 +1,24 @@
 import plotly.graph_objs as go
-from data.plays_by_artist import get_artists_plays_at_work
+from data.plays_by_artist import get_artists_plays_at_work, get_artists_plays
 from data.plays_by_artist import get_artists_plays_weekends
 from data.plays_by_artist import get_artists_plays_late_night
 import datetime
 
 
-def get_artists_plays_at_work_data(scrobbles, artists, top_n=8):
+def get_artists_plays_data(scrobbles, artists, top_n=8):
     start = scrobbles.index.min()[0]
     end = scrobbles.index.max()[0]
 
-    # last_monday = end - datetime.timedelta(days=end.weekday())
-    # start = last_monday
+    artists = (get_artists_plays(scrobbles, start, end)
+        .merge(artists[['URL', 'ImageURL']], how='left', left_index=True, right_index=True))
+    artists.head()
+
+    return artists.head(top_n).sort_values('PlayCount')
+
+
+def get_artists_plays_at_work_data(scrobbles, artists, top_n=8):
+    start = scrobbles.index.min()[0]
+    end = scrobbles.index.max()[0]
 
     artists = (get_artists_plays_at_work(scrobbles, start, end)
         .merge(artists[['URL', 'ImageURL']], how='left', left_index=True, right_index=True))
@@ -23,10 +31,6 @@ def get_artists_plays_on_weekends(scrobbles, artists, top_n=8):
     start = scrobbles.index.min()[0]
     end = scrobbles.index.max()[0]
 
-    # last_saturday = end - datetime.timedelta(days=end.weekday())
-    # start = last_saturday - datetime.timedelta(days=3)
-    # end = last_saturday + datetime.timedelta(days=1)
-
     artists = (get_artists_plays_weekends(scrobbles, start, end)
         .merge(artists[['URL', 'ImageURL']], how='left', left_index=True, right_index=True))
     artists.head()
@@ -37,9 +41,6 @@ def get_artists_plays_on_weekends(scrobbles, artists, top_n=8):
 def get_artists_plays_late_at_night(scrobbles, artists, top_n=8):
     start = scrobbles.index.min()[0]
     end = scrobbles.index.max()[0]
-
-    # last_monday = end - datetime.timedelta(days=end.weekday())
-    # start = last_monday
 
     artists = (get_artists_plays_late_night(scrobbles, start, end)
         .merge(artists[['URL', 'ImageURL']], how='left', left_index=True, right_index=True))
